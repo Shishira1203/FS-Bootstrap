9db5a86ac7e1c342d38f1fc703904b6b9135fa31
shishira1203 <shishir.karanth@gmail.com>
2020-07-22 00:23:41 +0530

    NPM Scripts Part-1

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $("#mycarousel").carousel({interval:2000 });
+    $("#carouselButton").click(function(){
+        if($("#carouselButton").children("span").hasClass("fa-pause")){
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass("fa-pause");
+            $("#carouselButton").children("span").addClass("fa-play");
+        }
+        else{
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass("fa-play");
+            $("#carouselButton").children("span").addClass("fa-pause");
+        }
+        
+    });
+    $("#login").click(function(){
+        $("#loginModal").modal("toggle");
+    });
+    $("#closebutton").click(function(){
+        $("#loginModal").modal("toggle");
+    });
+    $("#cancelbutton").click(function(){
+        $("#loginModal").modal("toggle");
+    });
+    $("#cancel").click(function(){
+        $("#reserveModal").modal("toggle");
+    });
+    $("#reservetable").click(function(){
+        $("#reserveModal").modal("toggle");
+    });
+    /*$("#carousel-pause").click(function(){
+        $("#mycarousel").carousel('pause');
+    });
+    $("#carousel-play").click(function(){
+        $("#mycarousel").carousel('cycle');
+    });*/
+});
